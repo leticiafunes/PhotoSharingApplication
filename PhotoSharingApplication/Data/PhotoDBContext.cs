430850c9653eed9ff4810e8edb8f85048769b97b
Leticiae <letyfunes@gmail.com>
2022-11-25 16:34:21 -0300

    Agregar archivos de proyecto.

---
+++ PhotoSharingApplication/Data/PhotoDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+using PhotoSharingApplication.Models;
+
+
+namespace PhotoSharingApplication.Data
+{
+    public class PhotoDBContext : DbContext
+
+    {
+        public PhotoDBContext() : base("keyPhotoDB") { }
+
+        public DbSet<Photo> Photos { get; set; }
+
+
+    }
+
+}
---
+++ PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PhotoSharingApplication.Models;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace PhotoSharingApplication.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int PhotoID { get; set; }
+
+        [Required]
+      
+    public string Title { get; set; }
+
+        [DisplayName("Picture")]
+        [MaxLength]
+
+public byte[] PhotoFile { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+public string ImageMimeType { get; set; }
+
+        [DataType(DataType.MultilineText)]
+	public string Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Created Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
+
+	public DateTime CreatedDate { get; set; }
+	public string UserName { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+
+
+    }
+}
